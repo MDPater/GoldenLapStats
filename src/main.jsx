bd413fa0222b6a452a853d70367d81ac5e8e1557
MDPater <depatermax@gmail.com>
2025-07-19 07:37:45 +0200

    adding analytics

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App.jsx";
 import Privacy from "./sites/Privacy.jsx";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -15,5 +16,6 @@ createRoot(document.getElementById("root")).render(
         <Route path="/privacy" element={<Privacy />} />
       </Routes>
     </Router>
+    <Analytics />
   </StrictMode>
 );
