948f9c9bd0ed0f743f98428a5840244b2b6b4626
MDPater <depatermax@gmail.com>
2025-07-21 00:53:08 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import App from "./App.jsx";
 import Privacy from "./sites/Privacy.jsx";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
